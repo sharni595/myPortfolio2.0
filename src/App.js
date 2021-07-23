2d544e57ec357c46dc6be6e274f423006bdef31b
Sharni Zaugg <sharni.greener@gmail.com>
2021-07-23 12:25:45 -0600

    add assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Nav from './components/Nav';
 
 function App() {
+
+  const [projects] = useState([
+    {
+      name: "Let's Get Physical",
+      description: "An app that allows a user to track their fitness progress over a six week period, specifically focusing on weight lifting.",
+      url: "https://sharni595.github.io/lets-get-physical/",
+      github: "https://github.com/sharni595/lets-get-physical",
+      image: "physical.png"
+    },
+    {
+      name: "Painterly",
+      description: "A social app that allows users to channel and share their creativity in one place. Users can both create and post art for others to view and comment on.",
+      url: "https://painter1y.herokuapp.com/",
+      github: "https://github.com/sharni595/painterly",
+      image: "painterly.png"
+    },
+    {
+      name: "Budget Tracker",
+      description: "A simple app that uses PWA conventions to allow users to track income and expenses",
+      url: "https://pure-bayou-78480.herokuapp.com/",
+      github: "https://github.com/sharni595/pwa-budget-tracker",
+      image: "budget-tracker.png"
+    },
+    {
+      name: "Generic Blog Site",
+      description: "My first attempt at using SQL to create a blog sharing app with user authentication, deployed to heroku",
+      url: "https://vast-peak-51798.herokuapp.com/",
+      github: "https://github.com/sharni595/cms-blog-site",
+      image: "blog-site.png"
+    },
+    {
+      name: "Node Taker",
+      description: "A note taking app creating using Node.js and SQL, deployed to heroku",
+      url: "https://node-maker.herokuapp.com/notes",
+      github: "https://github.com/sharni595/node-taker",
+      image: "node-taker.png"
+    },
+    {
+      name: "Cliche Weather App",
+      description: "A basic weather app creating to demonstrate my ability to incorporate APIs",
+      url: "https://sharni595.github.io/cliche-weather-app/",
+      github: "https://github.com/sharni595/cliche-weather-app",
+      image: "weather.png"
+    },
+
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
     </div>
   );
 }
