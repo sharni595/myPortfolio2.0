2906d28f5f8f4e18a043e5eacdc6eeb20a9f4809
Sharni Zaugg <sharni.greener@gmail.com>
2021-07-25 21:24:45 -0600

    resume download button

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { BsDownload } from 'react-icons/bs';
 
 function Resume() {
     return(
-        <div>Resume</div>
+        <div className="flex items-center my-64">
+            <a href={require("../../assets/resume/SharniResume.pdf")} download="SharniResume.pdf" className="flex flex-row items-center mx-auto p-4 resume text-2xl rounded-lg">
+                Download My Resume!
+                <BsDownload className="ml-2"/>
+            </a>
+        </div>
     );
 }
 
