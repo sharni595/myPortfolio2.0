3e93772f8fab4de166f6b0b99f400a5799f53fc5
Sharni Zaugg <sharni.greener@gmail.com>
2021-07-25 21:28:58 -0600

    fix resume download

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BsDownload } from 'react-icons/bs';
+import resume from '../../assets/resume/SharniResume.pdf';
 
 function Resume() {
     return(
         <div className="flex items-center my-64">
-            <a href={require("../../assets/resume/SharniResume.pdf")} download="SharniResume.pdf" className="flex flex-row items-center mx-auto p-4 resume text-2xl rounded-lg">
+            <a href={resume} download="SharniResume.pdf" className="flex flex-row items-center mx-auto p-4 resume text-2xl rounded-lg">
                 Download My Resume!
                 <BsDownload className="ml-2"/>
             </a>
