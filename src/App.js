4d5300c6f5ed667203f3e62c8a12e0d99f38119e
Sharni Zaugg <sharni.zaugg@iinteractive.com>
2023-03-08 10:48:41 -0700

    restyle contact page and make functional

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Nav from './components/Nav';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
-import Resume from './components/Resume';
 import Footer from './components/Footer';
 
 function App() {
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,93 +1,53 @@
-import React from 'react';
-//import { validateEmail } from '../../utils/helpers';
-import emailjs from 'emailjs-com';
-//temporarily removed useState for deployment purposes
+// import React, { useRef } from 'react';
+// //import { validateEmail } from '../../utils/helpers';
+// import emailjs from 'emailjs-com';
+// //temporarily removed useState for deployment purposes
+
+
+//      emailjs.sendForm('gmail', 'portfolio_contact', e.target, 'user_n9Ca98N2r2z38WCsdmkc6')
+
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+  const form = useRef();
 
-    function sendEmail(e) {
-        e.preventDefault();
-    
-        emailjs.sendForm('gmail', 'portfolio_contact', e.target, 'user_n9Ca98N2r2z38WCsdmkc6')
-          .then((result) => {
-              console.log(result.text);
-          }, (error) => {
-              console.log(error.text);
-          });
-          e.target.reset();
-    }
+  const sendEmail = (e) => {
+    e.preventDefault();
+    console.log('this works???')
 
-    // const [formState, setFormState] = useState({ name: '', email: '', message: ''});
-    // const { name, email, message } = formState;
+    emailjs.sendForm('service_6tlofpp', 'portfolio_contact', form.current, 'user_n9Ca98N2r2z38WCsdmkc6')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    e.target.reset();
+  };
 
-    // const [errorMessage, setErrorMessage] = useState('');
-
-    // function handleChange(e) {
-    //     if(e.target.name === 'email') {
-    //         const isValid = validateEmail(e.target.value);
-    //         console.log(isValid);
-    //         //isValid conditional statement
-    //         if(!isValid){
-    //             setErrorMessage('Your email is invalid');
-    //         } else {
-    //             setErrorMessage('');
-    //         }
-    //     } else {
-    //         if(!e.target.value.length) {
-    //             setErrorMessage(`${e.target.name} is required.`);
-    //         } else {
-    //             setErrorMessage('');
-    //         }
-    //     }
-
-    //     if(!errorMessage){
-    //         setFormState({...formState, [e.target.name]: e.target.value})
-    //     }
-    // }
-    // console.log('errorMessage', errorMessage);
-
-    // function handleSubmit(e) {
-    //     e.preventDefault();
-    //     console.log(formState);
-    // }
-
-    return (
-        <section>
-            <span>**This section is not yet functional. It is a current side project.</span>
-            <h1 className="text-5xl font-bold text-center p-4">Contact me</h1>
-            <div className="flex justify-center h-screen mx-auto">
-                <form id="contact-form" onSubmit={sendEmail}>
-                    <div>
-                        <label htmlFor="name">Name:</label>
-                        <input type="text" name="name" 
-                            
-                            className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300"
-                            />
-                    </div>
-                    <div>
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" name="email" 
-                            
-                            className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300"
-                            />
-                    </div>
-                    <div>
-                        <label htmlFor="message">Message:</label>
-                        <textarea rows="5" name="message" 
-                                
-                                className="resize-none border rounded-md block appearance-none placeholder-gray-500 placeholder-opacity-100 border-light-blue-400 w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300 h-56"
-                                />
-                    </div>
-                    {/* {errorMessage && (
-                        <div>
-                            <p className="error-text">{errorMessage}</p>
-                        </div>
-                    )} */}
-                    <button className="w-full bg-gray-600 text-white font-bold py-3 px-4 rounded text-2xl" type="submit">Submit</button>
-                </form>
+  return (
+    <view>
+      <h1 className="text-5xl font-bold text-center p-4">Say hi!</h1>
+      <div className="flex justify-center h-screen mx-auto">
+        <form ref={form} onSubmit={sendEmail}>
+          <div className='grid grid-cols-2 gap-4'>
+            <div>
+              <label>Your name</label>
+              <input type="text" name="name" className='block border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 focus:outline-none focus:ring-2 focus:ring-light-blue-300' />
             </div>
-        </section>
-    );
-}
-
+            <div className='order-last'>
+              <label>Your email</label>
+              <input type="email" name="email" className='block border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 focus:outline-none focus:ring-2 focus:ring-light-blue-300' />
+            </div>
+            <div className='row-span-2'>
+            <label>Message</label>
+              <textarea name="message" className='resize-none border rounded-md border-light-blue-400 w-full py-3 px-4 text-gray-700 focus:outline-none focus:ring-2 focus:ring-light-blue-300 h-36' />
+            </div>
+          </div>
+          <button className="w-full bg-gray-600 text-white font-bold py-3 px-4 rounded text-2xl my-4" type="submit">Send</button>
+        </form>
+      </div>
+    </view>
+  );
+};
 export default Contact;
